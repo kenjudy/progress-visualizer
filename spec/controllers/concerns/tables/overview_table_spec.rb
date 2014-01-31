214aeb9410a054fa3be12f24cfed63dcf42c56e8
Ken Judy <ken.judy@simonandschuster. com>
2014-01-30 20:20:01 -0500

    overview table under test

--- spec/controllers/concerns/tables/overview_table_spec.rb
+++ spec/controllers/concerns/tables/overview_table_spec.rb
@@ -1,23 +1,29 @@
 require 'spec_helper'
 
-describe Tables::OverviewTable do
+describe "Tables::OverviewTable" do
   include Trello::JsonData
 
-  let(:board) { Trello::Board.new(lists: [example_list_data], cards: [example_card_data]) }
-  let(:adapter) { Adpaters::TrelloAdapter }
+  let(:cards) do 
+    (0..1).map do
+      example_card_data({ "idList" => "5170058469d58225070003ce", "labels" => [{color: "blue", name: "Committed"}], "name" => "(3) Foo" })
+    end
+  end
+  
+  let(:board) { Trello::Board.new(lists: [example_list_data], cards: cards) }
+  let(:adapter) { Adapters::TrelloAdapter }
 
   before { adapter.stub(request_board: board) }
 
   context "current" do
-    subject { Tables::OverviewTable.current }
-    #it { should == nil }
+    subject { Tables::OverviewTable.current(adapter) }
+    its([:totals]) { should == {:total_stories=>2, :total_estimates=>6} }
   end
 
   context "update" do
+    let(:story) { FactoryGirl.build(:done_story) }
+    before { DoneStory.stub(find_or_initialize_by: story)}
+    after { Tables::OverviewTable.update(adapter) }
     
-    
- #   after { Tables::OverviewTable.update(adapter) }
-    
-#    it("should create done story") { expect_any_instance_of(DoneStory).to receive(:update_attributes).with() }
+    it("should create done story") { expect(story).to receive(:update_attributes).at_least(:once).with({:type_of_work=>"Committed", :status=>nil, :story=>"Foo", :estimate=>3.0}) }
   end
 end
