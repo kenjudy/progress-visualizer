b285ffc8ecd26b799e0d01f14d60a5ef1ddacf55
Ken Judy <ken.judy@simonandschuster. com>
2014-02-02 10:40:38 -0500

    attempt to fix bug in logic

--- app/models/done_story.rb
+++ app/models/done_story.rb
@@ -2,6 +2,6 @@ class DoneStory < ActiveRecord::Base
   
   def self.done_stories_data(range, include_current = false)
     offset = include_current ? 0 : 1.week
-    DoneStory.order("timestamp").where('timestamp > ?', Date.today - range.weeks - offset).to_a
+    DoneStory.order("timestamp").where('timestamp > ? and timestamp <= ?', Date.today - range.weeks - 1.week, Date.today - offset).to_a
   end
 end
