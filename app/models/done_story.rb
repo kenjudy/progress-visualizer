d7c7bca46a9b314d803dcde6a30eced24dd57a29
Ken Judy <ken.judy@simonandschuster. com>
2014-02-03 00:04:23 -0500

    play with ranges on yesterdays weather

--- app/models/done_story.rb
+++ app/models/done_story.rb
@@ -1,7 +1,8 @@
 class DoneStory < ActiveRecord::Base
   
   def self.done_stories_data(range, include_current = false)
+    beginning_of_iteration = Constants::CONFIG[:iteration_start]
     offset = include_current ? 0 : 1.week
-    DoneStory.order("timestamp").where('timestamp > ? and timestamp <= ?', Date.today - range.weeks - offset, Date.today - offset).to_a
+    DoneStory.order("timestamp").where('timestamp > ? and timestamp <= ?', beginning_of_iteration - range.weeks - offset, beginning_of_iteration - offset).to_a
   end
 end
