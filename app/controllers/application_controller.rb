112601bfb8c2e721b9abc5dc76715b7387f4e347
Ken Judy <ken.judy@simonandschuster. com>
2014-03-02 14:08:44 -0500

    complete help page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   skip_before_action :verify_authenticity_token, if: :json_request?
   before_filter :configure_permitted_parameters, if: :devise_controller?
-
+  
   protected
 
   def configure_permitted_parameters
