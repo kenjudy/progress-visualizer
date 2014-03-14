e968a39fa1e4bfee557de01f0241ee3b4e6beb47
Ken Judy <ken.judy@simonandschuster. com>
2014-03-14 19:49:31 -0400

    bash script to dump, download, import and scrub database

---
+++ db/scrub_db.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+heroku pgbackups:capture
+curl -o latest.dump `heroku pgbackups:url`
+pg_restore --verbose --clean --no-acl --no-owner -h localhost -d progress_visualizer_development latest.dump
+
+psql -h localhost -d progress_visualizer_development -c "delete from users where id<>2;"
+psql -h localhost -d progress_visualizer_development -c "delete from user_profiles where user_id <> 2;"
+psql -h localhost -d progress_visualizer_development -c "delete from burn_ups where user_profile_id not in (select id from user_profiles);"
+psql -h localhost -d progress_visualizer_development -c "delete from done_stories where user_profile_id not in (select id from user_profiles);"
+psql -h localhost -d progress_visualizer_development -c "delete from webhooks where user_profile_id not in (select id from user_profiles);"
+
+rm latest.dump
