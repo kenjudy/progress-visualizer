e200cbe18c5a14611011c0c75deef4b7f823636a
Ken Judy <ken.judy@simonandschuster. com>
2014-03-20 00:12:16 -0400

    fix specs

--- spec/models/user_profile_spec.rb
+++ spec/models/user_profile_spec.rb
@@ -55,17 +55,17 @@ describe UserProfile do
 
     context "user_profile" do
       it { should include("id,user_id,name,default,readonly_token,current_sprint_board_id,current_sprint_board_id_short,backlog_lists,done_lists,labels_types_of_work,duration,start_day_of_week,end_day_of_week,start_hour,end_hour,created_at,updated_at,start_date") }
-      it { should  =~ /1,1,ProgressVisualizer,1,6b6fe08bec7db2ccd331954b03a7800458fdb5732cd87f5defb0493021861d39,52e6778d5922b7e16a641e58,WWtHBRod,\"{\"\"52e6778d5922b7e16a641e59\"\":\"\"To Do\"\",\"\"52e6778d5922b7e16a641e5a\"\":\"\"Doing\"\"}\",\"{\"\"52e6778d5922b7e16a641e5b\"\":\"\"Done\"\"}\",red,7,1,0,9,0,\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} -\d{4},\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} -\d{4},/ }
+      it { should  =~ /\d+,\d+,ProgressVisualizer,1,6b6fe08bec7db2ccd331954b03a7800458fdb5732cd87f5defb0493021861d39,52e6778d5922b7e16a641e58,WWtHBRod,\"{\"\"52e6778d5922b7e16a641e59\"\":\"\"To Do\"\",\"\"52e6778d5922b7e16a641e5a\"\":\"\"Doing\"\"}\",\"{\"\"52e6778d5922b7e16a641e5b\"\":\"\"Done\"\"}\",red,7,1,0,9,0,\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} -\d{4},\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} -\d{4},/ }
     end
 
     context "done_stories" do
       it { should include("id,timestamp,iteration,type_of_work,status,story_id,story,estimate,created_at,updated_at,user_profile_id") }
-      it { should  =~ /1,2014-03-19,2014-03-17,Committed,This Sprint - Pending Deploy,\d+,Story name,1.0,\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} -\d{4},\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} -\d{4},1/ }
+      it { should  =~ /\d+,\d{4}-\d{2}-\d{2},\d{4}-\d{2}-\d{2},Committed,This Sprint - Pending Deploy,\d+,Story name,1.0,\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} -\d{4},\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} -\d{4},\d+/ }
     end
 
     context "burn_ups" do
       it { should include("id,timestamp,backlog,done,backlog_estimates,done_estimates,created_at,updated_at,user_profile_id") }
-      it { should =~ /1,2014-03-19 00:00:00 -0400,16,4,35.0,11.5,\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} -\d{4},\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} -\d{4},1/ }
+      it { should =~ /\d+,\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} -\d{4},16,4,35.0,11.5,\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} -\d{4},\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} -\d{4},\d+/ }
     end
   end
 end
