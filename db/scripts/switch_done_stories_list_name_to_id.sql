8ebba4d95032bf2eecb2604fe05c370f05c610f4
Ken Judy <ken@judykat.com>
2016-12-26 13:53:42 -0500

    Get ready to shutdown public instance

---
+++ db/scripts/switch_done_stories_list_name_to_id.sql
@@ -0,0 +1,3 @@
+update done_stories set status='5170058469d58225070003ce' where status='Ready for Sign-off';
+update done_stories set status='52a1fa40fc0416002b00644e' where status='This Sprint - Pending Deploy';
+update done_stories set status='52a1f9c76b8129814700d3f2' where status='This Sprint - Deployed/Done';
